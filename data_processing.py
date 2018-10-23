e599653a58cfd9387a06560b9ac743ae7c040c89
klin0816 <klin0816@gmail.com>
2018-10-23 16:33:17 +0800

    finish apriori

--- data_processing.py
+++ data_processing.py
@@ -7,15 +7,24 @@ if __name__ == '__main__':
     with open(file, "r") as f:
         temp = []
         start = 1
+        sstring = ''
         for line in f:
             s = line.rsplit()
             if int(s[1]) > start:
                 start += 1
                 big_list.append(temp)
                 temp = []
+                sstring += '\n'
             else:
                 temp.append(int(s[2]))
+                sstring = sstring + str(s[2]) + ' '
         #print("{}".format(s[2]))
-        print(big_list)
+        #print(big_list)
+
     with open(file + "_pre", "w") as fp:
         fp.write(str(big_list))
+
+    with open(file + "_p", "w") as ft:
+        ft.write(str(sstring))
+    
+    print("Proceed!")
